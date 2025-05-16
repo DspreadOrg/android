321a0767b009a05009a75447948d1d92844e101f
believemeng <945858825@qq.com>
2025-05-16 16:27:54 +0800

    feat: update the demo structure
    1. update the demo ui
    2. update the latest sdk
    BREAKING CHANGE: improve the demo logic

---
+++ pos_android_studio_demo/mvvmhabit/src/main/java/me/goldze/mvvmhabit/crash/DefaultErrorActivity.java
@@ -0,0 +1,191 @@
+/*
+ * Copyright 2014-2017 Eduard Ereza Martínez
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package me.goldze.mvvmhabit.crash;
+
+
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.DialogInterface;
+import android.content.res.TypedArray;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.TypedValue;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
+import me.goldze.mvvmhabit.R;
+import me.goldze.mvvmhabit.http.BaseResponse;
+import me.goldze.mvvmhabit.utils.ToastUtils;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+
+public final class DefaultErrorActivity extends AppCompatActivity {
+    private static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+    private static final String DINGTALK_WEBHOOK_URL = "https://oapi.dingtalk.com/robot/send?access_token=83e8afc691a1199c70bb471ec46d50099e6dd078ce10223bbcc56c0485cb5cc3";
+    @SuppressLint("PrivateResource")
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //This is needed to avoid a crash if the developer has not specified
+        //an app-level theme that extends Theme.AppCompat
+        TypedArray a = obtainStyledAttributes(R.styleable.AppCompatTheme);
+        if (!a.hasValue(R.styleable.AppCompatTheme_windowActionBar)) {
+            setTheme(R.style.Theme_AppCompat_Light_DarkActionBar);
+        }
+        a.recycle();
+
+        setContentView(R.layout.customactivityoncrash_default_error_activity);
+
+        //Close/restart button logic:
+        //If a class if set, use restart.
+        //Else, use close and just finish the app.
+        //It is recommended that you follow this logic if implementing a custom error activity.
+        Button restartButton = (Button) findViewById(R.id.customactivityoncrash_error_activity_restart_button);
+
+        final CaocConfig config = CustomActivityOnCrash.getConfigFromIntent(getIntent());
+
+        if (config.isShowRestartButton() && config.getRestartActivityClass()!=null) {
+            restartButton.setText(R.string.customactivityoncrash_error_activity_restart_app);
+            restartButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    CustomActivityOnCrash.restartApplication(DefaultErrorActivity.this, config);
+                }
+            });
+        } else {
+            restartButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    CustomActivityOnCrash.closeApplication(DefaultErrorActivity.this, config);
+                }
+            });
+        }
+
+        Button moreInfoButton = (Button) findViewById(R.id.customactivityoncrash_error_activity_more_info_button);
+
+        if (config.isShowErrorDetails()) {
+            moreInfoButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //We retrieve all the error data and show it
+
+                    AlertDialog dialog = new AlertDialog.Builder(DefaultErrorActivity.this)
+                            .setTitle(R.string.customactivityoncrash_error_activity_error_details_title)
+                            .setMessage(CustomActivityOnCrash.getAllErrorDetailsFromIntent(DefaultErrorActivity.this, getIntent()))
+                            .setPositiveButton(R.string.customactivityoncrash_error_activity_error_details_close, new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    uploadErrorInfoToDingTalk();
+                                }
+                            })
+                            .setNeutralButton(R.string.customactivityoncrash_error_activity_error_details_copy,
+                                    new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            copyErrorToClipboard();
+                                            Toast.makeText(DefaultErrorActivity.this, R.string.customactivityoncrash_error_activity_error_details_copied, Toast.LENGTH_SHORT).show();
+                                        }
+                                    })
+                            .show();
+                    TextView textView = (TextView) dialog.findViewById(android.R.id.message);
+                    textView.setTextSize(TypedValue.COMPLEX_UNIT_PX, getResources().getDimension(R.dimen.customactivityoncrash_error_activity_error_details_text_size));
+                }
+            });
+        } else {
+            moreInfoButton.setVisibility(View.GONE);
+        }
+
+        Integer defaultErrorActivityDrawableId = config.getErrorDrawable();
+        ImageView errorImageView = ((ImageView) findViewById(R.id.customactivityoncrash_error_activity_image));
+
+        if (defaultErrorActivityDrawableId != null) {
+            errorImageView.setImageDrawable(ResourcesCompat.getDrawable(getResources(), defaultErrorActivityDrawableId, getTheme()));
+        }
+    }
+
+    private void copyErrorToClipboard() {
+        String errorInformation = CustomActivityOnCrash.getAllErrorDetailsFromIntent(DefaultErrorActivity.this, getIntent());
+
+        ClipboardManager clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+        ClipData clip = ClipData.newPlainText(getString(R.string.customactivityoncrash_error_activity_error_details_clipboard_label), errorInformation);
+        clipboard.setPrimaryClip(clip);
+        uploadErrorInfoToDingTalk();
+    }
+
+    private void uploadErrorInfoToDingTalk() {
+        String errorInformation = CustomActivityOnCrash.getAllErrorDetailsFromIntent(DefaultErrorActivity.this, getIntent());
+        Log.i("test",errorInformation);
+        String json = "{\"msgtype\": \"markdown\", \"markdown\": {\"title\": \"issues\", \"text\": \"" + errorInformation.replace("\"", "\\\"") + "\"}}";
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                OkHttpClient client = new OkHttpClient();
+                RequestBody body = RequestBody.create(JSON, json);
+                Request request = new Request.Builder()
+                        .url(DINGTALK_WEBHOOK_URL)
+                        .post(body)
+                        .build();
+                try (Response response = client.newCall(request).execute()) {
+                    if (response.isSuccessful()) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(DefaultErrorActivity.this, "The error message was successfully uploaded", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    } else {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(DefaultErrorActivity.this, "Error message failed to be uploaded", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(DefaultErrorActivity.this, "There was an error with the network request!", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+            }
+        }).start();
+    }
+}
