70492f99ec320bc61ce4378d0800043223411f6e
mamengqiu <823163940@qq.com>
2025-10-15 17:29:23 +0800

    fix: add details for printing network requests

--- pos_android_studio_demo/pos_android_app/src/main/java/com/dspread/pos/common/http/RetrofitClient.java
+++ pos_android_studio_demo/pos_android_app/src/main/java/com/dspread/pos/common/http/RetrofitClient.java
@@ -1,12 +1,16 @@
 package com.dspread.pos.common.http;
 
+import com.dspread.pos.utils.TRACE;
 import com.google.gson.GsonBuilder;
 
 import me.goldze.mvvmhabit.utils.KLog;
+import okhttp3.Headers;
 import okhttp3.Interceptor;
 import okhttp3.Request;
 import okhttp3.Response;
+import okhttp3.ResponseBody;
 import okio.Buffer;
+import okio.BufferedSource;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 
 import java.io.IOException;
@@ -25,14 +29,56 @@ public class RetrofitClient {
             @Override
             public Response intercept(Chain chain) throws IOException {
                 Request request = chain.request();
+                
+                // 打印请求URL
+                TRACE.d("Request URL: " + request.url());
+                
+                // 打印请求头
+                Headers headers = request.headers();
+                StringBuilder headersBuilder = new StringBuilder("Request Headers:\n");
+                for (int i = 0; i < headers.size(); i++) {
+                    headersBuilder.append(headers.name(i)).append(": ").append(headers.value(i)).append("\n");
+                }
+                TRACE.d(headersBuilder.toString());
+                
+                // 打印请求体
                 if (request.body() != null) {
                     // 旧版本 OkHttp 读取请求体的方式
                     Buffer buffer = new Buffer();
                     request.body().writeTo(buffer);
                     String requestBody = buffer.readUtf8(); // 读取请求体内容
-                    KLog.d("Request Body: " + requestBody);
+                    TRACE.d("Request Body: " + requestBody);
                 }
-                return chain.proceed(request);
+                
+                // 执行请求并获取响应
+                Response response = chain.proceed(request);
+                
+                // 打印响应状态码和消息
+                TRACE.d("Response Status: " + response.code() + " " + response.message());
+                
+                // 打印响应头
+                Headers responseHeaders = response.headers();
+                StringBuilder responseHeadersBuilder = new StringBuilder("Response Headers:\n");
+                for (int i = 0; i < responseHeaders.size(); i++) {
+                    responseHeadersBuilder.append(responseHeaders.name(i)).append(": ").append(responseHeaders.value(i)).append("\n");
+                }
+                TRACE.d(responseHeadersBuilder.toString());
+                
+                // 打印响应体
+                ResponseBody responseBody = response.body();
+                if (responseBody != null) {
+                    // 复制响应体，避免流被消费后无法使用
+                    BufferedSource source = responseBody.source();
+                    source.request(Long.MAX_VALUE); // 请求所有数据
+                    Buffer buffer = source.buffer();
+                    
+                    // 克隆缓冲区，以便既能记录日志又不影响原响应
+                    Buffer cloneBuffer = buffer.clone();
+                    String responseBodyString = cloneBuffer.readUtf8();
+                    TRACE.d("Response Body: " + responseBodyString);
+                }
+                
+                return response;
             }
         };
 
