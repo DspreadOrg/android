dc131e5f79e295f655880d58856fa11b1701eca9
ouyangwanli <ouyangwanli@mail.dspread.com>
2025-11-19 18:08:19 +0800

    fix:update trade sdk version 8.1.2

--- pos_android_studio_demo/pos_android_app/src/main/java/com/dspread/pos/utils/HandleTxnsResultUtils.java
+++ pos_android_studio_demo/pos_android_app/src/main/java/com/dspread/pos/utils/HandleTxnsResultUtils.java
@@ -36,7 +36,7 @@ public class HandleTxnsResultUtils {
         viewModel.requestOnlineAuth(false, model);
     }
 
-    public static void handleMCRResult(PaymentResult result,Context context, ActivityPaymentBinding binding, PaymentViewModel viewModel) {
+    public static void handleMCRResult(PaymentResult result, Context context, ActivityPaymentBinding binding, PaymentViewModel viewModel) {
         Spanned receiptContent = ReceiptGenerator.generateMSRReceipt(result, "000013");
         binding.tvReceipt.setMovementMethod(LinkMovementMethod.getInstance());
         binding.tvReceipt.setText(receiptContent);
@@ -50,26 +50,26 @@ public class HandleTxnsResultUtils {
         viewModel.requestOnlineAuth(false, model);
     }
 
-    public static PaymentResult handleTransactionResult(PaymentResult paymentResult, Hashtable<String, String> decodeData){
-        paymentResult.setFormatID(decodeData.get("formatID") == null?"":decodeData.get("formatID"));
-        paymentResult.setMaskedPAN(decodeData.get("maskedPAN") == null? "":decodeData.get("maskedPAN"));
-        paymentResult.setExpiryDate(decodeData.get("expiryDate") == null? "":decodeData.get("expiryDate"));
-        paymentResult.setCardHolderName(decodeData.get("cardholderName") == null? "":decodeData.get("cardholderName"));
-        paymentResult.setServiceCode(decodeData.get("serviceCode") == null? "":decodeData.get("serviceCode"));
-        paymentResult.setTrack1Length(decodeData.get("track1Length") == null? "":decodeData.get("track1Length"));
-        paymentResult.setTrack2Length(decodeData.get("track2Length") == null? "":decodeData.get("track2Length"));
-        paymentResult.setTrack3Length(decodeData.get("track3Length") == null? "":decodeData.get("track3Length"));
-        paymentResult.setEncTracks(decodeData.get("encTracks") == null? "":decodeData.get("encTracks"));
-        paymentResult.setEncTrack1(decodeData.get("encTrack1") == null? "":decodeData.get("encTrack1"));
-        paymentResult.setEncTrack2(decodeData.get("encTrack2") == null? "":decodeData.get("encTrack2"));
-        paymentResult.setEncTrack3(decodeData.get("encTrack3") == null? "":decodeData.get("encTrack3"));
-        paymentResult.setPartialTrack(decodeData.get("partialTrack") == null? "":decodeData.get("partialTrack"));
-        paymentResult.setPinKsn(decodeData.get("pinKsn") == null? "":decodeData.get("pinKsn"));
-        paymentResult.setTrackksn(decodeData.get("trackksn") == null? "":decodeData.get("trackksn"));
-        paymentResult.setPinBlock(decodeData.get("pinBlock") == null? "":decodeData.get("pinBlock"));
-        paymentResult.setEncPAN(decodeData.get("encPAN") == null? "":decodeData.get("encPAN"));
-        paymentResult.setTrackRandomNumber(decodeData.get("trackRandomNumber") == null? "":decodeData.get("trackRandomNumber"));
-        paymentResult.setPinRandomNumber(decodeData.get("pinRandomNumber") == null? "":decodeData.get("pinRandomNumber"));
+    public static PaymentResult handleTransactionResult(PaymentResult paymentResult, Hashtable<String, String> decodeData) {
+        paymentResult.setFormatID(decodeData.get("formatID") == null ? "" : decodeData.get("formatID"));
+        paymentResult.setMaskedPAN(decodeData.get("maskedPAN") == null ? "" : decodeData.get("maskedPAN"));
+        paymentResult.setExpiryDate(decodeData.get("expiryDate") == null ? "" : decodeData.get("expiryDate"));
+        paymentResult.setCardHolderName(decodeData.get("cardholderName") == null ? "" : decodeData.get("cardholderName"));
+        paymentResult.setServiceCode(decodeData.get("serviceCode") == null ? "" : decodeData.get("serviceCode"));
+        paymentResult.setTrack1Length(decodeData.get("track1Length") == null ? "" : decodeData.get("track1Length"));
+        paymentResult.setTrack2Length(decodeData.get("track2Length") == null ? "" : decodeData.get("track2Length"));
+        paymentResult.setTrack3Length(decodeData.get("track3Length") == null ? "" : decodeData.get("track3Length"));
+        paymentResult.setEncTracks(decodeData.get("encTracks") == null ? "" : decodeData.get("encTracks"));
+        paymentResult.setEncTrack1(decodeData.get("encTrack1") == null ? "" : decodeData.get("encTrack1"));
+        paymentResult.setEncTrack2(decodeData.get("encTrack2") == null ? "" : decodeData.get("encTrack2"));
+        paymentResult.setEncTrack3(decodeData.get("encTrack3") == null ? "" : decodeData.get("encTrack3"));
+        paymentResult.setPartialTrack(decodeData.get("partialTrack") == null ? "" : decodeData.get("partialTrack"));
+        paymentResult.setPinKsn(decodeData.get("pinKsn") == null ? "" : decodeData.get("pinKsn"));
+        paymentResult.setTrackksn(decodeData.get("trackksn") == null ? "" : decodeData.get("trackksn"));
+        paymentResult.setPinBlock(decodeData.get("pinBlock") == null ? "" : decodeData.get("pinBlock"));
+        paymentResult.setEncPAN(decodeData.get("encPAN") == null ? "" : decodeData.get("encPAN"));
+        paymentResult.setTrackRandomNumber(decodeData.get("trackRandomNumber") == null ? "" : decodeData.get("trackRandomNumber"));
+        paymentResult.setPinRandomNumber(decodeData.get("pinRandomNumber") == null ? "" : decodeData.get("pinRandomNumber"));
 
         return paymentResult;
     }
@@ -120,21 +120,22 @@ public class HandleTxnsResultUtils {
         content.append("encPAN: ").append(encPAN).append("\n");
         content.append("trackRandomNumber: ").append(trackRandomNumber).append("\n");
         content.append("pinRandomNumber: ").append(pinRandomNumber).append("\n");
-
         return content.toString();
     }
 
     public static String getTradeResultMessage(QPOSService.DoTradeResult result, Context context) {
         switch (result) {
-            case NONE: return context.getString(R.string.no_card_detected);
-            case TRY_ANOTHER_INTERFACE: return context.getString(R.string.try_another_interface);
-            case NOT_ICC: return context.getString(R.string.card_inserted);
-            case BAD_SWIPE: return context.getString(R.string.bad_swipe);
-            case CARD_NOT_SUPPORT: return "GPO NOT SUPPORT";
-            case PLS_SEE_PHONE: return "PLS SEE PHONE, and pls wait for cardholder to confirm, then to try again";
-            case NFC_DECLINED: return context.getString(R.string.transaction_declined);
-            case NO_RESPONSE: return context.getString(R.string.card_no_response);
-            default: return context.getString(R.string.unknown_error);
+            //case NONE: return context.getString(R.string.no_card_detected);
+            //case TRY_ANOTHER_INTERFACE: return context.getString(R.string.try_another_interface);
+            // case NOT_ICC: return context.getString(R.string.card_inserted);
+            //case BAD_SWIPE: return context.getString(R.string.bad_swipe);
+            //case CARD_NOT_SUPPORT: return "GPO NOT SUPPORT";
+            case PLS_SEE_PHONE:
+                return "PLS SEE PHONE, and pls wait for cardholder to confirm, then to try again";
+            //case NFC_DECLINED: return context.getString(R.string.transaction_declined);
+            //case NO_RESPONSE: return context.getString(R.string.card_no_response);
+            default:
+                return context.getString(R.string.unknown_error);
         }
     }
 
@@ -149,31 +150,52 @@ public class HandleTxnsResultUtils {
     // get the TransactionType value
     public static QPOSService.TransactionType getTransactionType(String type) {
         if (type == null) return QPOSService.TransactionType.GOODS;
-
         switch (type) {
-            case "GOODS": return QPOSService.TransactionType.GOODS;
-            case "SERVICES": return QPOSService.TransactionType.SERVICES;
-            case "CASH": return QPOSService.TransactionType.CASH;
-            case "CASHBACK": return QPOSService.TransactionType.CASHBACK;
+            case "GOODS":
+                return QPOSService.TransactionType.GOODS;
+            case "SERVICES":
+                return QPOSService.TransactionType.SERVICES;
+            case "CASH":
+                return QPOSService.TransactionType.CASH;
+            case "CASHBACK":
+                return QPOSService.TransactionType.CASHBACK;
             case "PURCHASE_REFUND":
-            case "REFUND": return QPOSService.TransactionType.REFUND;
-            case "INQUIRY": return QPOSService.TransactionType.INQUIRY;
-            case "TRANSFER": return QPOSService.TransactionType.TRANSFER;
-            case "ADMIN": return QPOSService.TransactionType.ADMIN;
-            case "CASHDEPOSIT": return QPOSService.TransactionType.CASHDEPOSIT;
-            case "PAYMENT": return QPOSService.TransactionType.PAYMENT;
-            case "PBOCLOG||ECQ_INQUIRE_LOG": return QPOSService.TransactionType.PBOCLOG;
-            case "SALE": return QPOSService.TransactionType.SALE;
-            case "PREAUTH": return QPOSService.TransactionType.PREAUTH;
-            case "ECQ_DESIGNATED_LOAD": return QPOSService.TransactionType.ECQ_DESIGNATED_LOAD;
-            case "ECQ_UNDESIGNATED_LOAD": return QPOSService.TransactionType.ECQ_UNDESIGNATED_LOAD;
-            case "ECQ_CASH_LOAD": return QPOSService.TransactionType.ECQ_CASH_LOAD;
-            case "ECQ_CASH_LOAD_VOID": return QPOSService.TransactionType.ECQ_CASH_LOAD_VOID;
-            case "CHANGE_PIN": return QPOSService.TransactionType.UPDATE_PIN;
-            case "SALES_NEW": return QPOSService.TransactionType.SALES_NEW;
-            case "BALANCE_UPDATE": return QPOSService.TransactionType.BALANCE_UPDATE;
-            case "BALANCE": return QPOSService.TransactionType.BALANCE;
-            default: return QPOSService.TransactionType.GOODS;
+            case "REFUND":
+                return QPOSService.TransactionType.REFUND;
+            case "INQUIRY":
+                return QPOSService.TransactionType.INQUIRY;
+            case "TRANSFER":
+                return QPOSService.TransactionType.TRANSFER;
+            case "ADMIN":
+                return QPOSService.TransactionType.ADMIN;
+            case "CASHDEPOSIT":
+                return QPOSService.TransactionType.CASHDEPOSIT;
+            case "PAYMENT":
+                return QPOSService.TransactionType.PAYMENT;
+            case "PBOCLOG||ECQ_INQUIRE_LOG":
+                return QPOSService.TransactionType.PBOCLOG;
+            case "SALE":
+                return QPOSService.TransactionType.SALE;
+            case "PREAUTH":
+                return QPOSService.TransactionType.PREAUTH;
+            case "ECQ_DESIGNATED_LOAD":
+                return QPOSService.TransactionType.ECQ_DESIGNATED_LOAD;
+            case "ECQ_UNDESIGNATED_LOAD":
+                return QPOSService.TransactionType.ECQ_UNDESIGNATED_LOAD;
+            case "ECQ_CASH_LOAD":
+                return QPOSService.TransactionType.ECQ_CASH_LOAD;
+            case "ECQ_CASH_LOAD_VOID":
+                return QPOSService.TransactionType.ECQ_CASH_LOAD_VOID;
+            case "CHANGE_PIN":
+                return QPOSService.TransactionType.UPDATE_PIN;
+            case "SALES_NEW":
+                return QPOSService.TransactionType.SALES_NEW;
+            case "BALANCE_UPDATE":
+                return QPOSService.TransactionType.BALANCE_UPDATE;
+            case "BALANCE":
+                return QPOSService.TransactionType.BALANCE;
+            default:
+                return QPOSService.TransactionType.GOODS;
         }
     }
 
@@ -187,7 +209,7 @@ public class HandleTxnsResultUtils {
                 return context.getString(R.string.transaction_declined);
             case CANCEL:
                 return context.getString(R.string.transaction_cancel);
-            case CAPK_FAIL:
+           /* case CAPK_FAIL:
                 return context.getString(R.string.transaction_capk_fail);
             case NOT_ICC:
                 return context.getString(R.string.transaction_not_icc);
@@ -218,7 +240,7 @@ public class HandleTxnsResultUtils {
             case TRANS_TOKEN_INVALID:
                 return "TOKEN INVALID";
             case APP_BLOCKED:
-                return "APP BLOCKED";
+                return "APP BLOCKED";*/
             default:
                 return result.name();
         }
@@ -226,19 +248,28 @@ public class HandleTxnsResultUtils {
 
     public static String getDisplayMessage(QPOSService.Display displayMsg, Context context) {
         switch (displayMsg) {
-            case CLEAR_DISPLAY_MSG: return "";
-            case PLEASE_WAIT: return context.getString(R.string.wait);
-            case REMOVE_CARD: return context.getString(R.string.remove_card);
-            case TRY_ANOTHER_INTERFACE: return context.getString(R.string.try_another_interface);
-            case PROCESSING: return context.getString(R.string.processing);
-            case INPUT_PIN_ING: return "please input pin on pos";
+            // case CLEAR_DISPLAY_MSG: return "";
+            case PLEASE_WAIT:
+                return context.getString(R.string.wait);
+            case REMOVE_CARD:
+                return context.getString(R.string.remove_card);
+            // case TRY_ANOTHER_INTERFACE: return context.getString(R.string.try_another_interface);
+            case PROCESSING:
+                return context.getString(R.string.processing);
+            case INPUT_PIN_ING:
+                return "please input pin on pos";
             case INPUT_OFFLINE_PIN_ONLY:
-            case INPUT_LAST_OFFLINE_PIN: return "please input offline pin on pos";
-            case MAG_TO_ICC_TRADE: return "please insert chip card on pos";
-            case CARD_REMOVED: return "card removed";
-            case TRANSACTION_TERMINATED: return "transaction terminated";
-            case PlEASE_TAP_CARD_AGAIN: return context.getString(R.string.please_tap_card_again);
-            default: return "";
+            case INPUT_LAST_OFFLINE_PIN:
+                return "please input offline pin on pos";
+            case MAG_TO_ICC_TRADE:
+                return "please insert chip card on pos";
+            //  case CARD_REMOVED: return "card removed";
+            case TRANSACTION_TERMINATED:
+                return "transaction terminated";
+            case PlEASE_TAP_CARD_AGAIN:
+                return context.getString(R.string.please_tap_card_again);
+            default:
+                return "";
         }
     }
 
